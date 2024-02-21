b323b12ed5c2d3636b7f233ab799e446a0798e31
Aaliyah <145400122+MellowsWallflower@users.noreply.github.com>
2024-02-21 16:25:48 -0500

    Added Animal
    Animal Script

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    public string animalName;
+    public int age;
+    public float health;
+
+    public abstract void Move();
+    public abstract void Eat(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
